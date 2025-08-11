a8be6f751c7643db36c92feaf52150dc9a9464cf
madgey <dominic.emanuel@outlook.com>
2025-08-11 12:24:11 +0100

    new fix

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,13 +5,9 @@ export async function GET() {
   return NextResponse.json({
     version: 'vNext',
     name: 'Max Craic Poker Draw',
-    description: 'Enter now to win 5% if we cash — 10% if you recast',
-    image: 'https://max-craic-poker.vercel.app/api/frame-image2',
+    description: 'Enter now to win 5% if we cash, 10% if you recast',
+    image: 'https://max-craic-poker.vercel.app/frame.png',
     post_url: 'https://max-craic-poker.vercel.app/api/enter',
-    buttons: [
-      {
-        label: 'Enter Now'
-      }
-    ]
+    buttons: [{ label: 'Enter Now' }]
   });
 }
