277f7bc815b22a250c6dc90b7f8ae1e787b62b03
madgey <dominic.emanuel@outlook.com>
2025-08-16 13:03:39 +0100

    public fix7

--- app/head.tsx
+++ app/head.tsx
@@ -5,22 +5,19 @@ export default function Head() {
       <title>Max Craic Poker</title>
       <meta name="description" content="Enter draw, get paid if I cash." />
 
-      {/* Farcaster Frame meta */}
       <meta name="fc:frame" content="vNext" />
       <meta
         name="fc:frame:image"
-        content="https://max-craic-poker.vercel.app/api/frame-image2"
+        content="https://max-craic-poker.vercel.app/api/frame-image"
       />
       <meta name="fc:frame:button:1" content="Enter Now" />
       <meta
         name="fc:frame:post_url"
         content="https://max-craic-poker.vercel.app/api/enter"
       />
-
-      {/* Fallback */}
       <meta
         property="og:image"
-        content="https://max-craic-poker.vercel.app/api/frame-image2"
+        content="https://max-craic-poker.vercel.app/api/frame-image"
       />
     </>
   );
