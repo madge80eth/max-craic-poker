fa2dbfd2afb55c1d9ce34d587847094cc64fb628
madgey <dominic.emanuel@outlook.com>
2025-09-01 17:33:40 +0100

    more debugging 43

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   return NextResponse.json({
+    type: "frame",
+    version: "1",
     image: "https://max-craic-poker.vercel.app/api/frame-image",
-    buttons: [{ label: "Got POST" }],
+    buttons: [{ label: "Got POST" }]
   });
 }
